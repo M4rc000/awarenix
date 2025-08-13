bd8e233351bebc582e774998555450fd03b3fde1
Marco Antonio <marco.koten@i-3.co.id>
2025-08-13 14:50:37 +0700

    13.08.25

--- src/components/rolemanagement/CardHeader.tsx
+++ src/components/rolemanagement/CardHeader.tsx
@@ -7,7 +7,6 @@ export type CardHeaderRoleManagementProps = {
 
 export default function CardHeader({reloadTrigger}: CardHeaderRoleManagementProps) {
   const [totalUsers, setTotalUsers] = useState(0);
-  // const [growthDataUser, setGrowthDataUser] = useState(null);
   
   const fetchTotalRoles = useCallback(async () => {
   const API_URL = import.meta.env.VITE_API_URL;
@@ -17,74 +16,26 @@ export default function CardHeader({reloadTrigger}: CardHeaderRoleManagementProp
         headers: { Authorization: `Bearer ${token}` },
       });
       const data = await res.json();
-      if (data.Success && typeof data.Total === "number") {
-        setTotalUsers(data.Total);
+      
+      if (data.status == "success") {
+        setTotalUsers(data.total);
       }
     } catch (err) {
       console.error("Failed to fetch total user:", err);
     }
   }, []);
   
-  // const fetchGrowthData = useCallback(async () => {
-  // const API_URL = import.meta.env.VITE_API_URL;
-  // const token = localStorage.getItem("token");
-  //   try {
-  //     const res = await fetch(`${API_URL}/analytics/growth-percentage?type=users`, {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         'Authorization': `Bearer ${token}`
-  //       },
-  //     });
-  //     if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
-  //     const data = await res.json();
-  //     if (data.success && data.data) {
-  //       setGrowthDataUser(data.data);
-  //     }
-  //   } catch (error) {
-  //     console.error("❌ Fetch error:", error);
-  //   }
-  // }, []);
-
   useEffect(() => {
     fetchTotalRoles();
-    // fetchGrowthData();
 
     const intervalId = setInterval(() => {
       fetchTotalRoles();
-      // fetchGrowthData();
     }, 5000); 
 
     return () => clearInterval(intervalId);
 
   }, [reloadTrigger, fetchTotalRoles]); 
 
-  // const renderBadge = (growth: { growth_type: string; growth_percentage: number } | null) => {
-  //   if (!growth) return null;
-
-  //   const icon =
-  //     growth.growth_type === "increase" ? (
-  //       <TbArrowBigUpLine className="mr-1" />
-  //     ) : growth.growth_type === "decrease" ? (
-  //       <TbArrowBigDownLine className="mr-1" />
-  //     ) : (
-  //       <CgArrowsExchange className="mr-1 rotate-180" />
-  //     );
-
-  //   const color =
-  //     growth.growth_type === "increase"
-  //       ? "success"
-  //       : growth.growth_type === "decrease"
-  //       ? "error"
-  //       : "warning";
-
-  //   return (
-  //     <Badge color={color} className="dark:text-gray-400">
-  //       {icon}
-  //       {growth.growth_percentage.toFixed(2)}%
-  //     </Badge>
-  //   );
-  // };
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 w-full">
@@ -103,7 +54,6 @@ export default function CardHeader({reloadTrigger}: CardHeaderRoleManagementProp
             {totalUsers}
           </h4>
         </div>
-        {/* <div className="mt-2 flex justify-end">{renderBadge(growthDataUser)}</div> */}
       </div>
     </div>
   );
